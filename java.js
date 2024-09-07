75e941193d46a8624f8698fdf2748e1490508abf
Jaysitospams <jay.frances@icloud.com>
2024-09-07 10:44:35 -0700

    Add files via upload

---
+++ java.js
@@ -0,0 +1,31 @@
+let display = document.getElementById('display');
+let currentInput = '';
+
+function appendNumber(number) {
+    currentInput += number;
+    display.value = currentInput;
+}
+
+function appendOperator(operator) {
+    currentInput += ` ${operator} `;
+    display.value = currentInput;
+}
+
+function clearDisplay() {
+    currentInput = '';
+    display.value = '';
+}
+
+function calculateResult() {
+    try {
+        let result = eval(currentInput);
+        if (result === Infinity || isNaN(result)) {
+            throw new Error('Invalid operation');
+        }
+        display.value = result;
+        currentInput = result.toString();
+    } catch (error) {
+        display.value = 'Error';
+        currentInput = '';
+    }
+}
